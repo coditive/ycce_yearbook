fd02cff92225c6a73a4ff66a53b82a5654a7c169
syrous <syrouscodive@gmail.com>
2020-09-09 11:39:19 +0530

    Notification Service modified

--- app/src/main/java/com/syrous/ycceyearbook/util/NotificationService.kt
+++ app/src/main/java/com/syrous/ycceyearbook/util/NotificationService.kt
@@ -6,14 +6,12 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.syrous.ycceyearbook.data.local.UserSharedPrefStorage
-import com.syrous.ycceyearbook.model.Result.Error
 import com.syrous.ycceyearbook.model.Result.Success
 import timber.log.Timber
 
 class NotificationService: FirebaseMessagingService() {
 
     private lateinit var storage: UserSharedPrefStorage
-
 
     override fun onCreate() {
         super.onCreate()
@@ -37,19 +35,16 @@ class NotificationService: FirebaseMessagingService() {
     private fun sendTokenToServer(token: String) {
        val result = storage.getLoggedInUser()
         val db = FirebaseFirestore.getInstance()
+        val data = hashMapOf(
+            USER_NOTIFICATION_TOKEN to token,
+            UPDATE_TIMESTAMP to listOf(FieldValue.serverTimestamp())
+        )
         if(result is Success) {
             val user = result.data
-            val data = hashMapOf(
-                USER_NOTIFICATION_TOKEN to token,
-                UPDATE_TIMESTAMP to listOf(FieldValue.serverTimestamp())
-            )
             val query = db.collection(USER_COLLECTION).document(user.googleid)
             query.set(user)
             query.set(data)
-        } else if(result is Error) {
-            TODO("")
         }
-
     }
 
 }
